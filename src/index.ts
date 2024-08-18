8a9ec6e437cbb6f6236b69b3e1220e7db31c105d
priooods <priodwisembodo23@gmail.com>
2024-08-18 15:20:46 +0700

    [DEV][COMPANY] : CRUD Standart Company Module

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import http from "http";
 import env from "dotenv";
 import routerUser from "./module/User/Router/api.ts";
+import routerCompany from "./module/Company/Router/api.ts";
 const apps = express();
 const httpServer = http.createServer(apps);
 env.config();
@@ -11,6 +12,7 @@ const port = process.env.PORT ?? 8000;
 apps.use(bodyParser.urlencoded({ extended: false }));
 apps.use(bodyParser.json());
 apps.use('/user', routerUser);
+apps.use('/company', routerCompany);
 
 const callback = async () => {
   console.log(`Server started on http://localhost:${port}`);
